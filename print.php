714e20fbffb94389900c37990b6d568b8859db00
speedjin1995 <speedjin1995@gmail.com>
2023-09-03 14:10:45 +0800

    added print

---
+++ print.php
@@ -0,0 +1,260 @@
+<?php
+require_once 'php/db_connect.php';
+
+$productName = '';
+$productCode = '';
+$basis_weight = '';
+$width = '';
+$diameter = '';
+$class = '';
+$weight = '0.00kg';
+
+if(isset($_GET['product']) && $_GET['product'] != null && $_GET['product'] != ''){
+    $productName = (string)$_GET['product'];
+    $products = $db->query("SELECT * FROM products WHERE product_name = '".$productName."'");
+
+    if ($row5=mysqli_fetch_assoc($products)){
+        $productCode = $row5['product_code'];
+        $basis_weight = $row5['basis_weight'];
+        $width = $row5['width'];
+        $diameter = $row5['diameter'];
+        $class = $row5['class'];
+    }
+}
+
+if(isset($_GET['weight']) && $_GET['weight'] != null && $_GET['weight'] != ''){
+    $weight = (string)$_GET['weight'].' kg';
+}
+
+if(isset($_GET['date']) && $_GET['date'] != null && $_GET['date'] != ''){
+    $dateAndTime = (string)$_GET['date'];
+    list($datePart, $timePart) = explode(" ", $dateAndTime);
+    list($year, $month, $day) = explode("-", $datePart);
+    $date = "$day-$month-$year";
+    $time = $timePart;
+}
+?>
+
+<html>
+    <head>
+        <style>
+            @media print {
+                @page {
+                    margin-left: 0.5in;
+                    margin-right: 0.5in;
+                    margin-top: 0.1in;
+                    margin-bottom: 0.1in;
+                }
+                
+            } 
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                
+            } 
+            
+            .table th, .table td {
+                padding: 0.70rem;
+                vertical-align: top;
+                border-top: 1px solid #dee2e6;
+                
+            } 
+            
+            .table-bordered {
+                border: 1px solid #000000;
+            } 
+            
+            .table-bordered th, .table-bordered td {
+                border: 1px solid #000000;
+                font-family: sans-serif;
+            } 
+            
+            .row {
+                display: flex;
+                flex-wrap: wrap;
+                margin-top: 20px;
+            } 
+            
+            .col-md-3{
+                position: relative;
+                width: 25%;
+            }
+            
+            .col-md-9{
+                position: relative;
+                width: 75%;
+            }
+            
+            .col-md-7{
+                position: relative;
+                width: 58.333333%;
+            }
+            
+            .col-md-5{
+                position: relative;
+                width: 41.666667%;
+            }
+            
+            .col-md-6{
+                position: relative;
+                width: 50%;
+            }
+            
+            .col-md-4{
+                position: relative;
+                width: 33.333333%;
+            }
+            
+            .col-md-8{
+                position: relative;
+                width: 66.666667%;
+            }
+            
+            #footer {
+                position: fixed;
+                padding: 10px 10px 0px 10px;
+                bottom: 0;
+                width: 100%;
+                height: 30%;
+            }
+
+            #print-button {
+                position: fixed;
+                bottom: 10px;
+                left: 10px;
+                padding: 10px;
+                background-color: #007bff;
+                color: #fff;
+                cursor: pointer;
+                border-radius: 5px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <table class="table">
+            <tbody>
+                <tr style="border: 1px solid #000000;">
+                    <td style="width: 55%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 34px;font-family: sans-serif;font-weight: bold;">精牛原纸</span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 16px;font-family: sans-serif;">口 合格证</span><br>
+                            <span style="font-size: 14px;font-family: sans-serif;">Quality Certificate</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 16px;font-family: sans-serif;">Time : <?=$time ?></span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 16px;font-family: sans-serif;">DATE : <?=$date ?></span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:0px;"></td>
+                    <td style="width: 45%;border-top:0px;"></td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:0px;"></td>
+                    <td style="width: 45%;border-top:0px;"></td>
+                </tr>
+                <tr style="border: 1px solid #000000;">
+                    <td style="width: 55%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">产品编号 Product No.</span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">定量 BASIS WEIGHT C/M2</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:3px;">
+                        <img src="assets/barcode2.png" alt="Girl in a jacket" width="50%">
+                        <p style="margin-top: 0px;">
+                            <span style="font-size: 14px;font-family: sans-serif;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?=$productCode ?></span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;"><?=$basis_weight ?></span>
+                        </p>
+                    </td>
+                </tr>
+                <tr style="border: 1px solid #000000;">
+                    <td style="width: 55%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">宽度 WIDTH MM</span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">直径 DIAMETER MM</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;"><?=$width ?></span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 20pxs;font-family: sans-serif;"><?=$diameter ?></span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:0px;"></td>
+                    <td style="width: 45%;border-top:0px;"></td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:0px;"></td>
+                    <td style="width: 45%;border-top:0px;"></td>
+                </tr>
+                <tr style="border: 1px solid #000000;">
+                    <td style="width: 55%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">质量 WEIGHT KG</span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:0px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;">产品等级 PRODUCT CLASS</span>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 55%;border-top:3px;">
+                        <img src="assets/barcode2.png" alt="Girl in a jacket" width="50%">
+                        <p style="margin-top: 0px;">
+                            <span style="font-size: 14px;font-family: sans-serif;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?=$weight ?></span>
+                        </p>
+                    </td>
+                    <td style="width: 45%;border-top:3px;">
+                        <p>
+                            <span style="font-size: 20px;font-family: sans-serif;"><?=$class ?></span>
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div id="print-button" onclick="window.print()">
+            Print
+        </div>
+    </body>
+</html>
---
+++ weighingApp.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>SyncWeight </title>
+
+  <link rel="icon" href="assets/logoSmall.png" type="image">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- IonIcons -->
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck for checkboxes and radio inputs -->
+  
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- Bootstrap4 Duallistbox -->
+  <link rel="stylesheet" href="plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="plugins/toastr/toastr.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css?v=3.2.0">
+  
+  <style>
+    body {
+      background: #eee;
+      font-family: Assistant, sans-serif
+    }
+  
+    .cell-1 {
+      border-collapse: separate;
+      border-spacing: 0 4em;
+      background: #ffffff;
+      border-bottom: 5px solid transparent;
+      background-clip: padding-box;
+      cursor: pointer
+    }
+  
+    thead {
+      /* background: #dddcdc */
+      background-color: #007bff; 
+      color:white;
+    }
+  
+    .table-elipse {
+      cursor: pointer
+    }
+  
+    .expand-body {
+      -webkit-transition: all 0.3s ease-in-out;
+      -moz-transition: all 0.3s ease-in-out;
+      -o-transition: all 0.3s 0.1s ease-in-out;
+      transition: all 0.3s ease-in-out
+    }
+  
+    .row-child {
+      background-color: #000;
+    }
+
+    /*.hidden {
+      display: none !important;
+    }*/
+
+    div.loading{
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(16, 16, 16, 0.5);
+      z-index: 5;
+    }
+
+    @-webkit-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-webkit-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-moz-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-ms-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-moz-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-webkit-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @-o-keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    @keyframes uil-ring-anim {
+      0% {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    .uil-ring-css {
+      margin: auto;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      width: 200px;
+      height: 200px;
+    }
+
+    .uil-ring-css > div {
+      position: absolute;
+      display: block;
+      width: 160px;
+      height: 160px;
+      top: 20px;
+      left: 20px;
+      border-radius: 80px;
+      box-shadow: 0 6px 0 0 #ffffff;
+      -ms-animation: uil-ring-anim 1s linear infinite;
+      -moz-animation: uil-ring-anim 1s linear infinite;
+      -webkit-animation: uil-ring-anim 1s linear infinite;
+      -o-animation: uil-ring-anim 1s linear infinite;
+      animation: uil-ring-anim 1s linear infinite;
+    }
+  </style>
+</head>
+
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Weight Weighing</h1>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card card-primary">
+          <div class="card-body">
+            <table id="weightTable" class="table table-bordered table-striped display">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Customers</th>
+                  <th>Product</th>
+                  <th>Weight</th>
+                  <th>Created <br>Date & Time</th>
+                  <th></th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>   
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/jquery-validation/jquery.validate.min.js"></script>
+<!-- Bootstrap -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="dist/js/adminlte.js"></script>
+<!-- OPTIONAL SCRIPTS -->
+<script src="plugins/select2/js/select2.full.min.js"></script>
+<script src="plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/inputmask/jquery.inputmask.min.js"></script>
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/toastr/toastr.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script>
+// Values
+var controlflow = "None";
+var indicatorUnit = "kg";
+var weightUnit = "1";
+var rate = 1;
+var currency = "1";
+
+$(function () {
+  var table = $("#weightTable").DataTable({
+    "responsive": true,
+    "autoWidth": false,
+    'processing': true,
+    'serverSide': true,
+    'serverMethod': 'post',
+    'searching': true,
+    'order': [[ 1, 'asc' ]],
+    'columnDefs': [ { orderable: false, targets: [0] }],
+    'ajax': {
+        'url':'php/loadWeights.php'
+    },
+    'columns': [
+      { data: 'no' },
+      { data: 'customer_name' },
+      { data: 'product' },
+      { data: 'weight' },
+      { data: 'created_datetime' },
+      { 
+        className: 'dt-control',
+        orderable: false,
+        data: null,
+        render: function ( data, type, row ) {
+          return '<td class="table-elipse" data-toggle="collapse" data-target="#demo'+row.serialNo+'"><i class="fas fa-angle-down"></i></td>';
+        }
+      }
+    ],
+    "rowCallback": function( row, data, index ) {
+      $('td', row).css('background-color', '#E6E6FA');
+    },
+    "drawCallback": function(settings) {
+      
+    }
+  });
+
+  // Add event listener for opening and closing details
+  $('#weightTable tbody').on('click', 'td.dt-control', function () {
+    var tr = $(this).closest('tr');
+    var row = table.row( tr );
+
+    if ( row.child.isShown() ) {
+      // This row is already open - close it
+      row.child.hide();
+      tr.removeClass('shown');
+    }
+    else {
+      row.child( format(row.data()) ).show();tr.addClass("shown");
+    }
+  });
+});
+
+function format (row) {
+  return '<div class="row"><div class="col-md-3"><p>Status: '+row.status+
+  '</p></div><div class="col-md-3"><p>Shift: '+row.shift+
+  '</p></div><div class="col-md-3"><p>Staff Name: '+row.staff_name+
+  '</p></div></div><div class="row"><div class="col-3"><button type="button" class="btn btn-danger btn-sm" onclick="deactivate('+row.id+
+  ')"><i class="fas fa-trash"></i></button></div><div class="col-3"><button type="button" class="btn btn-info btn-sm" onclick="print('+row.id+
+  ')"><i class="fas fa-print"></i></button></div></div></div></div>'+
+  '</div>';
+}
+
+function formatNormal (row) {
+  return '<div class="row"><div class="col-md-3"><p>Average Cage Weight: '+row.average_cage+
+  ' kg</p></div><div class="col-md-3"><p>Average Bird Weight: '+row.average_bird+
+  ' kg</p></div><div class="col-md-3"><p>Minimum Weight: '+row.minimum_weight+
+  ' kg</p></div><div class="col-md-3"><p>Maximum Weight: '+row.maximum_weight+
+  ' kg</p></div></div><div class="row"><div class="col-3"><button type="button" class="btn btn-danger btn-sm" onclick="deactivate('+row.id+
+  ')"><i class="fas fa-trash"></i></button></div><div class="col-3"><button type="button" class="btn btn-info btn-sm" onclick="print('+row.id+
+  ')"><i class="fas fa-print"></i></button></div></div></div></div>'+
+  '</div>';
+}
+
+function deactivate(id) {
+  if (confirm('Are you sure you want to delete this items?')) {
+    $('#spinnerLoading').show();
+    $.post('php/deleteWeight.php', {userID: id}, function(data){
+      var obj = JSON.parse(data);
+
+      if(obj.status === 'success'){
+        toastr["success"](obj.message, "Success:");
+        $('#weightTable').DataTable().ajax.reload();
+      }
+      else if(obj.status === 'failed'){
+        toastr["error"](obj.message, "Failed:");
+      }
+      else{
+        toastr["error"]("Something wrong when activate", "Failed:");
+      }
+      $('#spinnerLoading').hide();
+    });
+  }
+}
+
+function print(id) {
+  $.post('php/print.php', {userID: id, file: 'weight'}, function(data){
+    var obj = JSON.parse(data);
+
+    if(obj.status === 'success'){
+      var printWindow = window.open('', '', 'height=400,width=800');
+      printWindow.document.write(obj.message);
+      printWindow.document.close();
+      setTimeout(function(){
+        printWindow.print();
+        printWindow.close();
+      }, 500);
+    }
+    else if(obj.status === 'failed'){
+      toastr["error"](obj.message, "Failed:");
+    }
+    else{
+      toastr["error"]("Something wrong when activate", "Failed:");
+    }
+  });
+}
+
+function portrait(id) {
+  $.post('php/printportrait.php', {userID: id, file: 'weight'}, function(data){
+    var obj = JSON.parse(data);
+
+    if(obj.status === 'success'){
+      var printWindow = window.open('', '', 'height=400,width=800');
+      printWindow.document.write(obj.message);
+      printWindow.document.close();
+      setTimeout(function(){
+        printWindow.print();
+        printWindow.close();
+      }, 500);
+    }
+    else if(obj.status === 'failed'){
+      toastr["error"](obj.message, "Failed:");
+    }
+    else{
+      toastr["error"]("Something wrong when activate", "Failed:");
+    }
+  });
+}
+</script>
